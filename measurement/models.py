718b0d27901607a845aaf945555950b3e0f79045
Andrey Gridnev <gridnevandr@yandex.ru>
2026-02-17 01:29:02 +0300

    Homework

--- measurement/models.py
+++ measurement/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from PIL import Image
 
 # TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length=300, blank=True)
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(
+        Sensor, on_delete=models.CASCADE, related_name='measurements')
+    temperature = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='measurements/', null=True, blank=True)
--- measurement/views.py
+++ measurement/views.py
@@ -1,2 +1,30 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
+from .models import Sensor, Measurement
+from .serializers import MeasurementSerializer, SensorSerializer, SensorDetailSerializer
+
+
+class SensorListCreateView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+    def get_serializer_class(self):
+        # Можно не переопределять, так как для list и create используется один сериализатор.
+        return super().get_serializer_class()
+
+
+class SensorRetrieveUpdateView(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+    def get_serializer_class(self):
+        if self.request.method in ['PUT', 'PATCH']:
+            return SensorSerializer
+        return SensorDetailSerializer
+
+
+class MeasurementCreateView(CreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
